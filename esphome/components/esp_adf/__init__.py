27c0f87e7efc1b5724ae81ccb6733b5e1eeed63c
Anderson <80588197+huishizhao@users.noreply.github.com>
2024-04-20 12:45:11 +0800

    Update __init__.py

--- esphome/components/esp_adf/__init__.py
+++ esphome/components/esp_adf/__init__.py
@@ -71,7 +71,7 @@ async def to_code(config):
 
     esp32.add_idf_component(
         name="esp-adf",
-        repo="https://github.com/espressif/esp-adf",
+        repo="https://github.com/huishizhao/esp-adf",
         path="components",
         ref="v2.5",
         components=["*"],
