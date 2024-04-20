73d6efb0d550bcdef0f9ed1e8efe9d88537eca7d
Anderson <80588197+huishizhao@users.noreply.github.com>
2024-04-20 10:37:31 +0800

    Update esp_adf_speaker.cpp

--- esphome/components/esp_adf/speaker/esp_adf_speaker.cpp
+++ esphome/components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -16,32 +16,17 @@
 namespace esphome {
 namespace esp_adf {
 
-static const size_t BUFFER_COUNT = 50;
+static const size_t BUFFER_COUNT = 25;
 
 static const char *const TAG = "esp_adf.speaker";
 
 void ESPADFSpeaker::setup() {
   ESP_LOGCONFIG(TAG, "Setting up ESP ADF Speaker...");
 
-  ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
-
-  this->buffer_queue_.storage = allocator.allocate(sizeof(StaticQueue_t) + (BUFFER_COUNT * sizeof(DataEvent)));
-  if (this->buffer_queue_.storage == nullptr) {
-    ESP_LOGE(TAG, "Failed to allocate buffer queue!");
-    this->mark_failed();
-    return;
-  }
-
-  this->buffer_queue_.handle =
-      xQueueCreateStatic(BUFFER_COUNT, sizeof(DataEvent), this->buffer_queue_.storage + sizeof(StaticQueue_t),
-                         (StaticQueue_t *) (this->buffer_queue_.storage));
-
+  this->buffer_queue_ = xQueueCreate(BUFFER_COUNT, sizeof(DataEvent));
   this->event_queue_ = xQueueCreate(20, sizeof(TaskEvent));
-  if (this->event_queue_ == nullptr) {
-    ESP_LOGW(TAG, "Could not allocate event queue.");
-    this->mark_failed();
-    return;
-  }
+
+  ESP_LOGCONFIG(TAG, "BufferQ:%p EventQ:%p", this->buffer_queue_, this->event_queue_);
 }
 
 void ESPADFSpeaker::start() { this->state_ = speaker::STATE_STARTING; }
@@ -60,67 +45,63 @@ void ESPADFSpeaker::player_task(void *params) {
   event.type = TaskEventType::STARTING;
   xQueueSend(this_speaker->event_queue_, &event, portMAX_DELAY);
 
-  i2s_driver_config_t i2s_config = {
-      .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
-      .sample_rate = 16000,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,
-      .dma_buf_count = 8,
-      .dma_buf_len = 1024,
-      .use_apll = false,
-      .tx_desc_auto_clear = true,
-      .fixed_mclk = 0,
-      .mclk_multiple = I2S_MCLK_MULTIPLE_256,
-      .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT,
-  };
-
   audio_pipeline_cfg_t pipeline_cfg = {
       .rb_size = 8 * 1024,
   };
   audio_pipeline_handle_t pipeline = audio_pipeline_init(&pipeline_cfg);
 
-  i2s_stream_cfg_t i2s_cfg = {
-      .type = AUDIO_STREAM_WRITER,
-      .i2s_config = i2s_config,
-      .i2s_port = I2S_NUM_0,
-      .use_alc = false,
-      .volume = 0,
-      .out_rb_size = I2S_STREAM_RINGBUFFER_SIZE,
-      .task_stack = I2S_STREAM_TASK_STACK,
-      .task_core = I2S_STREAM_TASK_CORE,
-      .task_prio = I2S_STREAM_TASK_PRIO,
-      .stack_in_ext = false,
-      .multi_out_num = 0,
-      .uninstall_drv = true,
-      .need_expand = false,
-      .expand_src_bits = I2S_BITS_PER_SAMPLE_16BIT,
-  };
+  i2s_stream_cfg_t i2s_cfg = {};
+  memset(&i2s_cfg, 0, sizeof(i2s_cfg));
+  i2s_cfg.type = AUDIO_STREAM_WRITER;
+  i2s_cfg.i2s_port = I2S_NUM_0;
+  i2s_cfg.use_alc = false;
+  i2s_cfg.volume = 0;
+  i2s_cfg.out_rb_size = I2S_STREAM_RINGBUFFER_SIZE;
+  i2s_cfg.task_stack = I2S_STREAM_TASK_STACK;
+  i2s_cfg.task_core = I2S_STREAM_TASK_CORE;
+  i2s_cfg.task_prio = I2S_STREAM_TASK_PRIO;
+  i2s_cfg.stack_in_ext = false;
+  i2s_cfg.multi_out_num = 0;
+  i2s_cfg.uninstall_drv = true;
+  i2s_cfg.need_expand = false;
+  i2s_cfg.expand_src_bits = I2S_BITS_PER_SAMPLE_16BIT;
+  i2s_cfg.buffer_len = I2S_STREAM_BUF_SIZE;
+  i2s_cfg.i2s_config.mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX);
+  i2s_cfg.i2s_config.sample_rate = 16000;
+  i2s_cfg.i2s_config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
+  i2s_cfg.i2s_config.channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT;
+  i2s_cfg.i2s_config.communication_format = I2S_COMM_FORMAT_STAND_I2S;
+  i2s_cfg.i2s_config.intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM;
+  i2s_cfg.i2s_config.dma_buf_count = 8;
+  i2s_cfg.i2s_config.dma_buf_len = 1024;
+  i2s_cfg.i2s_config.use_apll = false;
+  i2s_cfg.i2s_config.tx_desc_auto_clear = true;
+  i2s_cfg.i2s_config.fixed_mclk = 0;
+  i2s_cfg.i2s_config.bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT;
+
   audio_element_handle_t i2s_stream_writer = i2s_stream_init(&i2s_cfg);
-
-  rsp_filter_cfg_t rsp_cfg = {
-      .src_rate = 16000,
-      .src_ch = 1,
-      .dest_rate = 16000,
-      .dest_bits = 16,
-      .dest_ch = 2,
-      .src_bits = 16,
-      .mode = RESAMPLE_DECODE_MODE,
-      .max_indata_bytes = RSP_FILTER_BUFFER_BYTE,
-      .out_len_bytes = RSP_FILTER_BUFFER_BYTE,
-      .type = ESP_RESAMPLE_TYPE_AUTO,
-      .complexity = 2,
-      .down_ch_idx = 0,
-      .prefer_flag = ESP_RSP_PREFER_TYPE_SPEED,
-      .out_rb_size = RSP_FILTER_RINGBUFFER_SIZE,
-      .task_stack = RSP_FILTER_TASK_STACK,
-      .task_core = RSP_FILTER_TASK_CORE,
-      .task_prio = RSP_FILTER_TASK_PRIO,
-      .stack_in_ext = true,
-  };
-  audio_element_handle_t filter = rsp_filter_init(&rsp_cfg);
-
+  /*
+    rsp_filter_cfg_t rsp_cfg = {};
+    rsp_cfg.src_rate = 16000;
+    rsp_cfg.src_ch = 1;
+    rsp_cfg.src_bits = 16;
+    rsp_cfg.dest_rate = 16000;
+    rsp_cfg.dest_ch = 2;
+    rsp_cfg.dest_bits = 16;
+    rsp_cfg.mode = RESAMPLE_DECODE_MODE;
+    rsp_cfg.max_indata_bytes = RSP_FILTER_BUFFER_BYTE;
+    rsp_cfg.out_len_bytes = RSP_FILTER_BUFFER_BYTE;
+    rsp_cfg.type = ESP_RESAMPLE_TYPE_AUTO;
+    rsp_cfg.complexity = 2;
+    rsp_cfg.down_ch_idx = 0;
+    rsp_cfg.prefer_flag = ESP_RSP_PREFER_TYPE_SPEED;
+    rsp_cfg.out_rb_size = RSP_FILTER_RINGBUFFER_SIZE;
+    rsp_cfg.task_stack = RSP_FILTER_TASK_STACK;
+    rsp_cfg.task_core = RSP_FILTER_TASK_CORE;
+    rsp_cfg.task_prio = RSP_FILTER_TASK_PRIO;
+    rsp_cfg.stack_in_ext = true;
+    audio_element_handle_t filter = rsp_filter_init(&rsp_cfg);
+  */
   raw_stream_cfg_t raw_cfg = {
       .type = AUDIO_STREAM_WRITER,
       .out_rb_size = 8 * 1024,
@@ -128,7 +109,7 @@ void ESPADFSpeaker::player_task(void *params) {
   audio_element_handle_t raw_write = raw_stream_init(&raw_cfg);
 
   audio_pipeline_register(pipeline, raw_write, "raw");
-  audio_pipeline_register(pipeline, filter, "filter");
+  //  audio_pipeline_register(pipeline, filter, "filter");
   audio_pipeline_register(pipeline, i2s_stream_writer, "i2s");
 
   const char *link_tag[3] = {
@@ -148,7 +129,7 @@ void ESPADFSpeaker::player_task(void *params) {
   uint32_t last_received = millis();
 
   while (true) {
-    if (xQueueReceive(this_speaker->buffer_queue_.handle, &data_event, 0) != pdTRUE) {
+    if (xQueueReceive(this_speaker->buffer_queue_, &data_event, 0) != pdTRUE) {
       if (millis() - last_received > 500) {
         // No audio for 500ms, stop
         break;
@@ -158,7 +139,7 @@ void ESPADFSpeaker::player_task(void *params) {
     }
     if (data_event.stop) {
       // Stop signal from main thread
-      while (xQueueReceive(this_speaker->buffer_queue_.handle, &data_event, 0) == pdTRUE) {
+      while (xQueueReceive(this_speaker->buffer_queue_, &data_event, 0) == pdTRUE) {
         // Flush queue
       }
       break;
@@ -193,12 +174,12 @@ void ESPADFSpeaker::player_task(void *params) {
   xQueueSend(this_speaker->event_queue_, &event, portMAX_DELAY);
 
   audio_pipeline_unregister(pipeline, i2s_stream_writer);
-  audio_pipeline_unregister(pipeline, filter);
+  //  audio_pipeline_unregister(pipeline, filter);
   audio_pipeline_unregister(pipeline, raw_write);
 
   audio_pipeline_deinit(pipeline);
   audio_element_deinit(i2s_stream_writer);
-  audio_element_deinit(filter);
+  //  audio_element_deinit(filter);
   audio_element_deinit(raw_write);
 
   event.type = TaskEventType::STOPPED;
@@ -219,7 +200,7 @@ void ESPADFSpeaker::stop() {
   this->state_ = speaker::STATE_STOPPING;
   DataEvent data;
   data.stop = true;
-  xQueueSendToFront(this->buffer_queue_.handle, &data, portMAX_DELAY);
+  xQueueSendToFront(this->buffer_queue_, &data, portMAX_DELAY);
 }
 
 void ESPADFSpeaker::watch_() {
@@ -263,10 +244,6 @@ void ESPADFSpeaker::loop() {
 }
 
 size_t ESPADFSpeaker::play(const uint8_t *data, size_t length) {
-  if (this->is_failed()) {
-    ESP_LOGE(TAG, "Failed to play audio, speaker is in failed state.");
-    return 0;
-  }
   if (this->state_ != speaker::STATE_RUNNING && this->state_ != speaker::STATE_STARTING) {
     this->start();
   }
@@ -278,7 +255,7 @@ size_t ESPADFSpeaker::play(const uint8_t *data, size_t length) {
     size_t to_send_length = std::min(remaining, BUFFER_SIZE);
     event.len = to_send_length;
     memcpy(event.data, data + index, to_send_length);
-    if (xQueueSend(this->buffer_queue_.handle, &event, 0) != pdTRUE) {
+    if (xQueueSend(this->buffer_queue_, &event, 0) != pdTRUE) {
       return index;  // Queue full
     }
     remaining -= to_send_length;
@@ -287,7 +264,7 @@ size_t ESPADFSpeaker::play(const uint8_t *data, size_t length) {
   return index;
 }
 
-bool ESPADFSpeaker::has_buffered_data() const { return uxQueueMessagesWaiting(this->buffer_queue_.handle) > 0; }
+bool ESPADFSpeaker::has_buffered_data() const { return uxQueueMessagesWaiting(this->buffer_queue_) > 0; }
 
 }  // namespace esp_adf
 }  // namespace esphome
