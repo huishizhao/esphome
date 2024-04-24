345438746b14d90679c2e00ed2a808cda248cbc6
Anderson <80588197+huishizhao@users.noreply.github.com>
2024-04-24 22:17:46 +0800

    Update esp_adf_microphone.cpp

--- esphome/components/esp_adf/microphone/esp_adf_microphone.cpp
+++ esphome/components/esp_adf/microphone/esp_adf_microphone.cpp
@@ -124,6 +124,7 @@ void ESPADFMicrophone::read_task(void *params) {
       .multi_out_num = 0,
       .uninstall_drv = true,
       .need_expand = false,
+      .buffer_len = I2S_STREAM_BUF_SIZE,  
   };
   audio_element_handle_t i2s_stream_reader = i2s_stream_init(&i2s_cfg);
 
